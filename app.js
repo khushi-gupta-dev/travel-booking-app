0cdea94a42b10dbf0542aff243dfb8ab02e626f6
khushigupt1 <khush9gupt1@gmail.com>
2026-04-23 03:36:59 +0530

    added error handling

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const methodOverride = require("method-override");
 const MONGO_URI = "mongodb://localhost:27017/wanderlust";
 const ejsMate = require("ejs-mate");
 
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError.js");
+
+const { listingSchema } = require("./schema.js");
 
 main()
   .then(() => {
@@ -60,38 +64,88 @@ app.get("/listings/:id", async (req, res) => {
   res.render("listings/show.ejs", { listing });
 });
 
-app.post("/listings", async (req, res) => {
-  // const { title, description, image, price,country, location } = req.body;
+
+
+
+
+app.post("/listings",wrapAsync( async (req, res,next) => {
+  if(!req.body.listing){
+    throw new ExpressError(400, "Invalid listing data");
+  }
+     // const { title, description, image, price,country, location } = req.body;
   let listing = req.body.listing;
   const newListing = new Listing(listing);
+  if(!newListing.description){
+    throw new ExpressError(400, "Description is a required field");
+  }
+  if(!newListing.title){
+    throw new ExpressError(400, "Title is a required field");
+  }
+  if(!newListing.price){
+    throw new ExpressError(400, "Price is a required field");
+  }
+  if(!newListing.location){
+    throw new ExpressError(400, "Location is a required field");
+  }
   await newListing.save();
   res.redirect("/listings");
-});
+  
+}));
 
 
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
    
-});
+}));
 
 
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+  if(!req.body.listing){
+    throw new ExpressError(400, "Invalid listing data");
+  }
   let { id } = req.params;
  
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
-});
+}));
 
 
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id",wrapAsync( async (req, res) => {
   let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log("Deleted listing:", deletedListing);
   res.redirect("/listings");
-});
+}));
+
+
+
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page Not Found!"));
+})
+
+app.use((err, req, res, next) => {
+  let { statusCode=500, message="Something went wrong" } = err; 
+  // res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs",{err});
+})
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// joi used for schema validation {schema for server side validation of listing data}
