0b5a69f41f3652a8b5cebf0b737a369070cfeb0c
khushi-gupta-dev <khushi.gupta28.dev@gmail.com>
2026-04-30 02:23:01 +0530

    restructure reviews

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing");
 
 const path = require("path");
 const methodOverride = require("method-override");
 const MONGO_URI = "mongodb://localhost:27017/wanderlust";
 const ejsMate = require("ejs-mate");
 
-const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require("./utils/ExpressError.js");
 
-const { listingSchema , reviewSchema } = require("./schema.js");
-const Review = require("./models/review.js");
-
 const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 main()
   .then(() => {
@@ -52,81 +48,21 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const validateListing = (req, res, next) => {
-  let { error } = listingSchema.validate(req.body);
-  
-  if (error) {
-  let errMsg = error.details.map(el => el.message).join(",");
-  throw new ExpressError(400, errMsg);
-}else{
-  next();
-}
- }
-
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {  let errMsg = error.details.map(el => el.message).join(",");
-  throw new ExpressError(400, errMsg);
-}else{
-  next();
-}
-}
- 
-
-
 app.use("/listings", listings);
-
-
-
-//reviews
-//post route for reviews
-
-app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
-
-  listing.reviews.push(newReview);
-  await newReview.save();
-  await listing.save();
-
-  res.redirect(`/listings/${listing._id}`);
-}));
-
-
-//delete review route
-
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
-  let { id, reviewId } = req.params;
-  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/listings/${id}`);
-} ));
+app.use("/listings/:id/reviews", reviews);
 
 app.use((req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
-})
+});
 
 app.use((err, req, res, next) => {
-  let { statusCode=500, message="Something went wrong" } = err; 
+  let { statusCode = 500, message = "Something went wrong" } = err;
   // res.status(statusCode).send(message);
-  res.status(statusCode).render("error.ejs",{err});
-})
+  res.status(statusCode).render("error.ejs", { err });
+});
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // joi used for schema validation {schema for server side validation of listing data}
